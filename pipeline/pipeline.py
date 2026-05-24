45c24a99e0b8a0d2b51ef216cbcd27ba6968795a
translatinam-cmd <translatinam@gmail.com>
2026-05-24 16:44:43 +0000

    dataframe update

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -8,7 +8,8 @@ print("arguments", sys.argv)
 
 month = int(sys.argv[1])
 
-df = pd.DataFrame({"A": [1, 2], "B": [3, 4]})
+df = pd.DataFrame({"day": [1, 2], "num_passengers": [3, 4]})
+df['month']=month
 print(df.head())
 
 print(f"Hello pipeline, month={month}")
