74e5714788303165612568a3a5413f3a875e7060
translatinam-cmd <translatinam@gmail.com>
2026-05-24 17:25:11 +0000

    parquet

--- pipeline/pipeline.py
+++ pipeline/pipeline.py
@@ -1,8 +1,10 @@
-import sys
+import sys  
+print(sys.executable)
 
 import pandas as pd
 
-
+import os
+os.makedirs("data", exist_ok=True)
 
 print("arguments", sys.argv)
 
@@ -11,5 +13,7 @@ month = int(sys.argv[1])
 df = pd.DataFrame({"day": [1, 2], "num_passengers": [3, 4]})
 df['month']=month
 print(df.head())
+
+df.to_parquet(f"data/pipeline_output_{month}.parquet")
 
 print(f"Hello pipeline, month={month}")
